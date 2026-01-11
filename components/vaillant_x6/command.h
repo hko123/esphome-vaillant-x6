5ee29da7dcef3d6aec4339ec24e8caaad4c2cace
hko123 <61331675+hko123@users.noreply.github.com>
2026-01-11 07:36:45 +0100

    Update command.h
    neue Klasse GetAnalogueValue1ByteCommand

--- components/vaillant_x6/command.h
+++ components/vaillant_x6/command.h
@@ -1,18 +1,14 @@
 #pragma once
-
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "request_response_handler.h"
-
 namespace esphome {
 namespace vaillant_x6 {
-
 class VaillantX6Command : public Command {
   public:
     virtual ~VaillantX6Command() = default;
     virtual void process_response(uint8_t* response) = 0;
     virtual int get_expected_response_length();
-
     virtual int get_interval() {
       return 1;
     }
@@ -20,15 +16,23 @@ class VaillantX6Command : public Command {
   private:
     int expected_response_length = 0;
 };
-
 class GetAnalogueValue2BytesCommand : public VaillantX6Command {
   public:
     virtual void process_response(uint8_t* response) override;
-
     virtual int get_interval() override {
         return interval;
     }
+    int interval = 1;
+    sensor::Sensor* sensor;
+};
 
+// NEUE KLASSE:
+class GetAnalogueValue1ByteCommand : public VaillantX6Command {
+  public:
+    virtual void process_response(uint8_t* response) override;
+    virtual int get_interval() override {
+        return interval;
+    }
     int interval = 1;
     sensor::Sensor* sensor;
 };
@@ -36,11 +40,9 @@ class GetAnalogueValue2BytesCommand : public VaillantX6Command {
 class GetOnOffStatusCommand : public VaillantX6Command {
   public:
     virtual void process_response(uint8_t* response) override;
-
     virtual int get_interval() override {
         return interval;
     }
-
     std::string object_id;
     std::string icon;
     std::string sensor_name;
@@ -48,6 +50,5 @@ class GetOnOffStatusCommand : public VaillantX6Command {
     uint8_t on_value = 0x01;
     binary_sensor::BinarySensor* sensor;
 };
-
 } // namespace vaillant_x6
 } // namespace esphome
